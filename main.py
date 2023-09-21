b6b1bd131003a197857f7fa7d13ce752a8844a18
ahmedsamiabdelhady <a.samiabdelhady@gmail.com>
2023-09-21 18:37:33 +0300

    Updated by adding exception

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pandas
+
+
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dict = {
     row.letter:row.code for (index, row) in data.iterrows()
 }
 
-username = input("Enter a word: ").upper()
-code_list = [nato_dict[letter] for letter in username]
-print(code_list)
+Error= True
+while Error:
+    username = input("Enter a word: ").upper()
+    try:
+        code_list = [nato_dict[letter] for letter in username]
+    except KeyError:
+        print("Sorry, Only letters in the alphabet please.")
+    else:
+        print(code_list)
+        Error=False
 
